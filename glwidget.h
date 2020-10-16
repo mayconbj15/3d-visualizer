f706b9f137155f3b316b485e513607af29611f43
mayconbj15 <mayconbj15@gmail.com>
2020-10-15 23:02:18 -0300

    first commit

---
+++ glwidget.h
@@ -0,0 +1,16 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QWidget>
+
+class GLWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GLWidget(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GLWIDGET_H
