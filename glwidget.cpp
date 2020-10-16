f68d4937f7dfd9a5656effb1490853037bad320d
mayconbj15 <mayconbj15@gmail.com>
2020-10-16 00:05:56 -0300

    desgosto

--- glwidget.cpp
+++ glwidget.cpp
@@ -2,5 +2,26 @@
 
 GLWidget::GLWidget(QWidget *parent) : QWidget(parent)
 {
+}
 
+void GLWidget::initializeGL(){
+
+}
+
+void GLWidget::resizeGL(int width, int height){
+
+}
+
+void GLWidget::paintGL(){
+    glEnd();
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void GLWidget::toggleBackgroundColor(bool toBlack){
+    if(toBlack)
+        glClearColor(0,0,0,1);
+    else
+        glClearColor(1,1,1,1);
+
+    //updateGL();
 }
--- glwidget.h
+++ glwidget.h
@@ -1,6 +1,7 @@
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
 
+#include <QtOpenGL>
 #include <QWidget>
 
 class GLWidget : public QWidget
@@ -8,6 +9,14 @@ class GLWidget : public QWidget
     Q_OBJECT
 public:
     explicit GLWidget(QWidget *parent = nullptr);
+
+public slots:
+    void toggleBackgroundColor(bool toBlack);
+
+protected:
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void paintGL();
 
 signals:
 
