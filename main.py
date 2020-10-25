1f9ad1bd5430a32116d3245fe50bc02996ef3950
xXHachimanXx <globaldep90@gmail.com>
2020-10-25 14:14:00 -0200

    Adicionando interface primária.

--- main.py
+++ main.py
@@ -2,26 +2,43 @@ import pygame
 import pygame_gui
 import view_3d_object as view3d
 
-
 pygame.init()
+display = (400, 600)
 
-pygame.display.set_caption('Quick Start')
-window_surface = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Quick Start")
+window_surface = pygame.display.set_mode(display)
 
-background = pygame.Surface((800, 600))
-background.fill(pygame.Color('#000000'))
+background = pygame.Surface(display)
+background.fill(pygame.Color("#000000"))
 
-manager = pygame_gui.UIManager((800, 600))
+manager = pygame_gui.UIManager(display)
 
-hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
-                                            text='Say Hello',
-                                            manager=manager)
+hello_button = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((150, 170), (100, 50)), text="Show", manager=manager
+)
+
+openFileButton = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((150, 240), (100, 50)), text="Open File", manager=manager
+)
+
+shaderLabel = pygame_gui.elements.UILabel(
+    relative_rect=pygame.Rect((100, 310), (200, 50)), text="Choose a shader:", manager=manager
+)
+
+chooseShaderLeftButton = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((100, 380), (90, 50)), text="Prev.", manager=manager
+)
+
+chooseShaderRightButton = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((210, 380), (90, 50)), text="Next", manager=manager
+)
+
 
 clock = pygame.time.Clock()
 is_running = True
 
 while is_running:
-    time_delta = clock.tick(60)/1000.0
+    time_delta = clock.tick(60) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
@@ -29,6 +46,7 @@ while is_running:
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == hello_button:
+                    #view3d.main()                    
                     view3d.main()
 
         manager.process_events(event)
